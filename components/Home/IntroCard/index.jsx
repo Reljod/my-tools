6045e36e0ba5e17ca6542a95cd12202996c8be9d
Reljod Oreta <oretareljod@gmail.com>
2022-05-03 17:05:59 +0800

    Add initial testing and improve ui

--- components/Home/IntroCard/index.jsx
+++ components/Home/IntroCard/index.jsx
@@ -1,14 +1,19 @@
 import intro from '@Home/IntroCard/Introcard.module.css';
+import styles from './Introcard.module.css'
 
 
 export default function IntroCard() {
 	return (
-		<div className="inner cover">
-			<span className='cover-heading d-flex'>
-				Hi, I am
-				<h1>Jod</h1>
-			</span>
-			<h4 className='lead'>Full-Stack Developer | Freelancer</h4>
+		<div className="d-flex flex-column align-items-center justify-content-end">
+			<div>
+				<span className={`lead display-4 ${styles.greetings}`}>
+					Hi, I am 
+				</span>
+				<span className={`lead display-1 mx-2 ${styles.name}`}>
+					Jod
+				</span>
+			</div>
+			<h4 className={`${styles.title}`}>Full-Stack Developer | Freelancer</h4>
 			<div className='introSocial'></div>
 		</div>
 	)
--- pages/index.js
+++ pages/index.js
@@ -5,14 +5,19 @@ import styles from 'styles/Home.module.css'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div className='container p-3 vh-100'>
       <Head>
         <title>My Tools</title>
         <meta name="description" content="Compilation of my personal tools." />
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"></meta>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <IntroCard/>
+      <div className='row h-100 align-content-center justify-content-center'>
+        <div className='col-12'>
+          <IntroCard/>
+        </div>
+      </div>
+      
     </div>
   )
 }
